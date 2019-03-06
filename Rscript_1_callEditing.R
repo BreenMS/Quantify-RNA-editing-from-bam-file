aaf8ede98b640c6e52dc5a1ccf314c19e7e9b9d8
Michael S Breen <michael.breen@mssm.edu>
2019-03-06 14:32:03 -0500

    Update Rscript_1_callEditing.R

--- Rscript_1_callEditing.R
+++ Rscript_1_callEditing.R
@@ -1,6 +1,7 @@
-setwd("/sc/orga/projects/buxbaj01a/Breen/NIMH/")
+setwd("")
 
-file_list=read.table("BamFiles.txt", stringsAsFactors=FALSE) #a text file listing all mapped bam files
+#text file listing all mapped bam files in one column
+file_list=read.table("BamFiles.txt", stringsAsFactors=FALSE) 
 files=basename(file_list$V1)
 full_run_input <- NULL
 full_run_input <- paste('#!/bin/bash\n', sep="")
@@ -15,18 +16,18 @@ writeLines(paste('
 #!/bin/bash
 #BSUB -W 08:00
 #BSUB -n 5
-#BSUB -q premium
-#BSUB -P acc_buxbaj01a
-#BSUB -cwd /sc/orga/projects/buxbaj01a/Breen/NIMH/
-#BSUB -o /sc/orga/projects/buxbaj01a/Breen/NIMH/', file_name1, '.edit.log.out
-#BSUB -e /sc/orga/projects/buxbaj01a/Breen/NIMH/', file_name1, '.edit.log.err
-#BSUB -u michael.breen@mssm.edu
+#BSUB -q XYZ
+#BSUB -P XYZ
+#BSUB -cwd XYZ
+#BSUB -o setwd', file_name1, '.edit.log.out
+#BSUB -e setwd', file_name1, '.edit.log.err
+#BSUB -u XYZ
 #BSUB -R rusage[mem=14000]
 #BSUB -R span[hosts=1]
 #BSUB -L /bin/bash
 
 module load samtools
-cd /sc/orga/projects/buxbaj01a/Breen/NIMH 
+cd setwd/ 
 
 perl Step1_Query_Editing_Level.pl Radar_extended.txt ', file_name1, ' ', file_name1,'.editing.txt                                         #Identify RNA editing sites
 perl Step2_OverallEditing.pl ',file_name1,'.editing.txt > ', file_name1,'.overallEditing.txt                                              #Quantify overall RNA editing per sample
